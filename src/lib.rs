9c81f11fb3848cf403c3bc37e6038ec230c984c9
kmaitreys <km.maitrey@gmail.com>
2024-12-24 23:54:19 +0530

    feat: enhance run function to return `Result` and improve error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use io::Config;
-
+use std::fs;
+use std::{error::Error, path::Path};
 pub mod config;
 pub mod constants;
 pub mod dims;
@@ -139,61 +140,175 @@ pub struct Cell {
     pub centre: [f64; dims::N_DIMS],
 }
 
-pub fn run(path: &str) {
+pub fn run(path: &str) -> Result<(), Box<dyn Error>> {
     println!("Welcome to citrus, A verstalie line modelling engine based on LIME.");
     // Load the configuration file
-    let input_data = match Config::from_toml_file(path) {
-        Ok(data) => data, // Bind the successfully parsed data to `input_data`
-        Err(e) => {
-            eprintln!("Error reading the input file: {}", e);
-            return; // Exit early if there’s an error
-        }
-    };
+    println!("Loading configuration file: {}", path);
+    let input_data = Config::from_toml_file(path)?;
 
     // Extract parameters now that we are outside the match block
-    let pars = input_data.parameters;
-    let config = config::ConfigInfo::default();
+    let inpars = input_data.parameters;
+    let mut par = config::ConfigInfo::default();
 
     let imgs = input_data.images;
-    println!("Images: {:?}", imgs);
-    println!("Parameters: {:?}", pars);
-    println!("Config: {:?}", config);
 
-    // // Map pars to config
-    // config.radius = pars.radius;
-    // config.min_scale = pars.min_scale;
-    // config.p_intensity = pars.p_intensity;
-    // config.sink_points = pars.sink_points;
-    // config.sampling_algorithm = pars.sampling_algorithm;
-    // config.sampling = pars.sampling;
-    // config.lte_only = pars.lte_only;
-    // config.init_lte = pars.init_lte;
-    // config.cmb_temp = pars.cmb_temp;
-    // config.blend = pars.blend;
-    // config.anti_alias = pars.anti_alias;
-    // config.polarization = pars.polarization;
-    // config.nthreads = pars.nthreads;
-    // config.nsolve_iters = pars.nsolve_iters;
-    // config.ray_trace_algorithm = pars.ray_trace_algorithm;
-    // config.reset_rng = pars.reset_rng;
-    // config.do_solve_rte = pars.do_solve_rte;
-    // config.dust = pars.dust;
-    // config.output_file = pars.output_file;
-    // config.binoutput_file = pars.binoutput_file;
-    // config.restart = pars.restart;
-    // config.grid_file = pars.grid_file;
-    // config.pre_grid = pars.pre_grid;
-    // config.grid_in_file = pars.grid_in_file;
+    // Map pars to config
+    par.radius = inpars.radius;
+    par.min_scale = inpars.min_scale;
+    par.p_intensity = inpars.p_intensity;
+    par.sink_points = inpars.sink_points;
+    par.sampling_algorithm = inpars.sampling_algorithm;
+    par.sampling = inpars.sampling;
+    par.lte_only = inpars.lte_only;
+    par.init_lte = inpars.init_lte;
+    par.cmb_temp = inpars.cmb_temp;
+    par.blend = inpars.blend;
+    par.anti_alias = inpars.anti_alias;
+    par.polarization = inpars.polarization;
+    par.nthreads = inpars.nthreads;
+    par.nsolve_iters = inpars.nsolve_iters;
+    par.ray_trace_algorithm = inpars.ray_trace_algorithm;
+    par.reset_rng = inpars.reset_rng;
+    par.do_solve_rte = inpars.do_solve_rte;
+    par.dust = inpars.dust;
+    par.output_file = inpars.output_file;
+    par.binoutput_file = inpars.binoutput_file;
+    par.restart = inpars.restart;
+    par.grid_file = inpars.grid_file;
+    par.pre_grid = inpars.pre_grid;
+    par.grid_in_file = inpars.grid_in_file;
 
-    // config.ncell = config.p_intensity + config.sink_points;
-    // config.radius_squ = config.radius * config.radius;
-    // config.min_scale_squ = config.min_scale * config.min_scale;
-    // config.n_solve_iters_done = 0;
-    // config.use_abun = true;
-    // config.data_flags = 0;
+    par.grid_out_files = inpars
+        .grid_out_files
+        .iter()
+        .take(NUM_OF_GRID_STAGES)
+        .filter(|filename| !filename.is_empty())
+        .cloned()
+        .collect();
+    par.write_grid_at_stage.fill(0.0);
 
-    // config.grid_density_global_max = 1.;
-    // config.num_densities = 0;
+    if par.pre_grid.is_empty() && par.restart {
+        par.n_species = 0;
+        par.grid_data_file.clear();
+    } else {
+        par.n_species = inpars
+            .mol_data_file
+            .iter()
+            .take(MAX_NUM_OF_SPECIES)
+            .filter(|s| !s.is_empty())
+            .count() as i32;
+
+        let num_grid_data_files = inpars
+            .grid_data_file
+            .iter()
+            .take(MAX_NUM_OF_SPECIES)
+            .filter(|s| !s.is_empty())
+            .count() as i32;
+
+        if num_grid_data_files == 0 {
+            par.grid_data_file.clear();
+        } else if num_grid_data_files != par.n_species {
+            return Err("Number of grid data files different from number of species.".into());
+        } else {
+            par.grid_data_file = inpars
+                .grid_data_file
+                .iter()
+                .take(par.n_species as usize)
+                .flat_map(|s| s.parse::<f64>())
+                .collect();
+        }
+    }
+
+    if par.n_species <= 0 {
+        par.mol_data_file.clear();
+    } else {
+        par.mol_data_file = inpars
+            .mol_data_file
+            .iter()
+            .take(par.n_species as usize)
+            .filter(|filename| !filename.is_empty())
+            .cloned()
+            .collect();
+
+        for filename in &par.mol_data_file {
+            let path = Path::new(filename);
+            if path.exists() {
+                match fs::metadata(&path) {
+                    Ok(metadata) => {
+                        if metadata.len() == 0 {
+                            println!("File {} is empty.", filename);
+                        } else {
+                            println!("File {} exists and is not empty.", filename);
+                        }
+                    }
+                    Err(err) => {
+                        println!("Error accessing file {}: {}", filename, err);
+                    }
+                }
+            }
+        }
+    }
+
+    par.collisional_partner_ids = inpars
+        .collisional_partner_ids
+        .iter()
+        .take(MAX_NUM_OF_COLLISIONAL_PARTNERS)
+        .cloned()
+        .collect();
+
+    par.nmol_weights = inpars
+        .nmol_weights
+        .iter()
+        .take(MAX_NUM_OF_COLLISIONAL_PARTNERS)
+        .cloned()
+        .collect();
+
+    par.collisional_partner_names = inpars
+        .collisional_partner_names
+        .iter()
+        .take(MAX_NUM_OF_COLLISIONAL_PARTNERS)
+        .cloned()
+        .collect();
+
+    par.collisional_partner_mol_weights = inpars
+        .collisional_partner_mol_weights
+        .iter()
+        .take(MAX_NUM_OF_COLLISIONAL_PARTNERS)
+        .cloned()
+        .collect();
+
+    par.grid_density_max_values = inpars
+        .grid_density_max_values
+        .iter()
+        .take(MAX_NUM_HIGH)
+        .cloned()
+        .collect();
+
+    par.grid_density_max_locations = inpars
+        .grid_density_max_locations
+        .iter()
+        .take(MAX_NUM_HIGH)
+        .cloned()
+        .collect::<Vec<[f64; 3]>>();
+
+    println!("{:?}", par.grid_density_max_values); // TODO: Set default values
+                                                   // totally forgot ::facepalm::
+
+    let mut i = 0;
+    while i < MAX_NUM_HIGH && par.grid_density_max_values[i] >= 0.0 {
+        i += 1;
+    }
+    par.num_grid_density_maxima = i as i32;
+
+    par.ncell = par.p_intensity + par.sink_points;
+    par.radius_squ = par.radius * par.radius;
+    par.min_scale_squ = par.min_scale * par.min_scale;
+    par.n_solve_iters_done = 0;
+    par.use_abun = true;
+    par.data_flags = 0;
+
+    par.grid_density_global_max = 1.;
+    par.num_densities = 0;
 
     // if !config.do_pregrid || config.restart {
     //     config.num_densities = 0;
@@ -201,6 +316,7 @@ pub fn run(path: &str) {
     //         if let Err(e) = count_density_columns();
     //     }
     // }
+    Ok(())
 }
 
 // Define a struct to hold the configuration
