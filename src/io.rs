add1c72b8bb0b45cd6b7273f168c127acdbf05cd
kmaitreys <km.maitrey@gmail.com>
2024-12-25 16:58:09 +0530

    impl: new `Default` for `io` structs

--- src/io.rs
+++ src/io.rs
@@ -1,3 +1,5 @@
+use crate::constants::LOCAL_CMB_TEMP_SI;
+use crate::MAX_NUM_OF_COLLISIONAL_PARTNERS;
 use serde_derive::Deserialize;
 use std::collections::HashMap;
 use std::fs;
@@ -30,6 +32,7 @@ pub struct InputParams {
     pub reset_rng: bool,
     pub do_solve_rte: bool,
     pub nmol_weights: Vec<f64>,
+    pub dust_weights: Vec<f64>,
     pub grid_density_max_locations: Vec<[f64; 3]>,
     pub grid_density_max_values: Vec<f64>,
     pub collisional_partner_mol_weights: Vec<f64>,
@@ -40,7 +43,48 @@ pub struct InputParams {
     pub grid_out_files: Vec<String>,
 }
 
-#[derive(Debug, Default, Deserialize)]
+impl Default for InputParams {
+    fn default() -> Self {
+        InputParams {
+            radius: 0.0,
+            min_scale: 0.0,
+            p_intensity: 0,
+            sink_points: 0,
+            dust: String::new(),
+            output_file: String::new(),
+            binoutput_file: String::new(),
+            grid_file: String::new(),
+            pre_grid: String::new(),
+            restart: false,
+            grid_in_file: String::new(),
+            collisional_partner_ids: vec![0; MAX_NUM_OF_COLLISIONAL_PARTNERS],
+            nmol_weights: vec![-1.0; MAX_NUM_OF_COLLISIONAL_PARTNERS],
+            dust_weights: vec![-1.0; MAX_NUM_OF_COLLISIONAL_PARTNERS],
+            collisional_partner_mol_weights: vec![-1.0; MAX_NUM_OF_COLLISIONAL_PARTNERS],
+            grid_density_max_values: vec![-1.0; MAX_NUM_OF_COLLISIONAL_PARTNERS],
+            grid_density_max_locations: vec![[0.0; 3]; MAX_NUM_OF_COLLISIONAL_PARTNERS],
+            cmb_temp: LOCAL_CMB_TEMP_SI,
+            lte_only: 0,
+            init_lte: 0,
+            sampling_algorithm: 0,
+            sampling: 2,
+            blend: 0,
+            anti_alias: 1,
+            polarization: 0,
+            nthreads: 1,
+            nsolve_iters: 0,
+            ray_trace_algorithm: 0,
+            reset_rng: false,
+            do_solve_rte: false,
+            grid_out_files: vec![String::new(); 3],
+            mol_data_file: vec![String::new(); 3],
+            grid_data_file: vec![String::new(); 3],
+            collisional_partner_names: vec![String::new(); MAX_NUM_OF_COLLISIONAL_PARTNERS],
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
 #[serde(default)] // This ensures `Default::default()` is used for missing fields.
 pub struct Image {
     pub nchan: i32,
@@ -67,6 +111,32 @@ pub struct Image {
 // Implementing a constant for default_angle, as Rust does not support inline static expressions in structs.
 impl Image {
     pub const DEFAULT_ANGLE: f64 = -999.0;
+}
+
+impl Default for Image {
+    fn default() -> Self {
+        Image {
+            nchan: 0,
+            trans: -1,
+            mol_i: -1,
+            vel_res: -1.0,
+            img_res: -1.0,
+            pixels: -1,
+            unit: 0,
+            freq: -1.0,
+            bandwidth: -1.0,
+            filename: String::new(),
+            units: String::new(),
+            source_velocity: 0.0,
+            theta: 0.0,
+            phi: 0.0,
+            inclination: Image::DEFAULT_ANGLE,
+            position_angle: Image::DEFAULT_ANGLE,
+            azimuth: Image::DEFAULT_ANGLE,
+            distance: -1.0,
+            do_interpolate_vels: false,
+        }
+    }
 }
 
 #[derive(Debug, Deserialize)]
