e8ac6d541754fcd90c6e97d8a6efc25d2dd3cbc3
kmaitreys <km.maitrey@gmail.com>
2024-12-29 17:35:29 +0530

    feat: define some grid related functions

--- src/grid.rs
+++ src/grid.rs
@@ -1,3 +1,9 @@
+use rgsl::rng::algorithms as GSLRngAlgorithms;
+use rgsl::Rng as GSLRng;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use crate::{config::ConfigInfo, Grid, Populations, FIX_RANDOM_SEEDS};
+
 pub struct LinkType {
     pub id: u32,
     pub gis: [u32; 2],
@@ -31,4 +37,48 @@ pub struct KeywordType {
     pub float_value: f64,
     pub double_value: f64,
     pub bool_value: bool,
+}
+
+pub fn set_default_grid(num_points: usize, num_species: usize) -> Vec<Grid> {
+    let mut gp = Vec::new();
+    for _ in 0..num_points {
+        let mut g = Grid::default();
+        if num_species > 0 {
+            g.mol
+                .get_or_insert_with(Vec::new)
+                .push(Populations::default());
+        } else {
+            g.mol = None;
+        }
+        gp.push(g);
+    }
+
+    gp
+}
+
+pub fn pre_define(par: &mut ConfigInfo, gp: &mut Vec<Grid>) {
+    let ran_opt = GSLRng::new(GSLRngAlgorithms::ranlxs2());
+    match ran_opt {
+        Some(mut ran) => {
+            if FIX_RANDOM_SEEDS {
+                ran.set(6611304);
+            } else {
+                ran.set(
+                    SystemTime::now()
+                        .duration_since(UNIX_EPOCH)
+                        .expect("Time went backwards")
+                        .as_secs() as usize,
+                );
+            }
+        }
+        None => {
+            panic!("Failed to create random number generator");
+        }
+    }
+
+    par.num_densities = 1;
+
+    for i in 0..par.ncell {
+        gp[i].dens = vec![0.0; par.num_densities];
+    }
 }
