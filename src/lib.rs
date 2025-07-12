30827783be45957b29a3f70c9fa09234955e18b0
kmaitreys <km.maitrey@gmail.com>
2025-07-12 13:49:49 +0530

    add link to book

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@
 //! is an excitation and radiation transfer code that can be used to predict line and continuum radiation
 //! from an astronomical source. The code uses unstructured 3D Delaunay grids for photon transport and
 //! accelerated Lambda Iteration for population calculations.
+//!
+//! For a detailed theoretical description of the code, please read the [book](https://prosialab.github.io/books/citrus/).
 
 pub mod collparts;
 pub mod config;
@@ -14,11 +16,11 @@ pub mod defaults;
 pub mod engine;
 pub mod grid;
 pub mod interface;
-pub mod model;
 pub mod io;
 pub mod lines;
 pub mod macros;
 pub mod messages;
+pub mod model;
 pub mod pops;
 pub mod raytrace;
 pub mod solver;
